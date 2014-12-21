5a11eea5122fcb2a2723355b7545d6317c1e50eb
biswajitbiee <biswajitbiee@users.noreply.github.com>
2014-12-21 19:31:35 +0530

    Created by Coding Ground Client

---
+++ Practice1/Algorithm.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Algorithm
+{
+public:
+    static void replaceSpaces(string& input);
+    
+    static int CompressedLength(const string& input);
+    
+    static void CompressString(string& input);
+};
