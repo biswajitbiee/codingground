0f285cc4e729e6b3511e236d340a4fcfff3233ec
biswajitbiee <biswajitbiee@users.noreply.github.com>
2014-12-21 15:11:45 +0530

    Created by Coding Ground Client

---
+++ PracticeChapter1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Algorithm.h"
+
+using namespace std;
+
+int main()
+{
+    string input = "This is a string";
+    replaceSpaces(input);
+    cout <<input<<endl;
+    
+    return 0;
+}
